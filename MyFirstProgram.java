cf4dc2eb2dcde9fd321941e9a7489a22a18d8d07
Alan <90580788+atcwarthon@users.noreply.github.com>
2023-04-11 11:19:08 -0700

    Add files via upload
    My first java program (lab 01)

---
+++ MyFirstProgram.java
@@ -0,0 +1,18 @@
+
+package WarthonAlan_lab1;
+
+/**
+ *
+ * @author C0523935
+ */
+public class MyFirstProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        	System.out.println("Preferred Pronoun: He \nPreferred name: Alan");
+    }
+    
+}
